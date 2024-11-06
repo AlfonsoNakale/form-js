1ae20d2a74bdb177c0eaf21b652060efe663b999
AlfonsoNakale <98756653+AlfonsoNakale@users.noreply.github.com>
2024-11-07 01:51:09 +0200

    @

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,15 @@ export default defineConfig({
     outDir: 'dist',
     rollupOptions: {
       output: {
+        format: 'iife',
         entryFileNames: 'assets/[name].js',
         chunkFileNames: 'assets/[name].js',
         assetFileNames: 'assets/[name].[ext]',
+        inlineDynamicImports: true,
       },
     },
+    target: 'es2015',
+    sourcemap: true,
   },
   server: {
     port: 3000,
